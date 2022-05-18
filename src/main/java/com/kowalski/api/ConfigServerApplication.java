8175b7e369916d6dd0cb85441382d896fb9bb2f6
paavosoeiro <paavomsoeiro@gmail.com>
2022-05-18 14:34:44 -0300

    Upload config-server repo

---
+++ src/main/java/com/kowalski/api/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.kowalski.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(ConfigServerApplication.class, args);
+		 new SpringApplicationBuilder(ConfigServerApplication.class).run(args);
+	}
+
+}
